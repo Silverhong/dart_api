8ed5658fea36a845c5650b56f432c097a372d374
Carrot <carrot@Carrots-MacBook-Pro.local>
2022-06-25 10:59:32 +0700

    fix request CORS

--- lib/server.dart
+++ lib/server.dart
@@ -11,7 +11,15 @@ void main(List<String> arguments) async {
   Singleton singleton = Singleton.instance;
   Singleton.instance.connectDb();
 
+  const port = 8081;
   final app = Router();
+
+  // CORS Settings
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type',
+  };
 
   app.mount('/user', UserRoute().router);
 
@@ -19,6 +27,22 @@ void main(List<String> arguments) async {
 
   app.mount('/product', ProductRoute().router);
 
-  await io.serve(app, '51.79.251.248', 8081);
-  // await io.serve(app, 'localhost', 8081);
+  // Set CORS headers with every request
+  final handler = Pipeline().addMiddleware((innerHandler) {
+    return (request) async {
+      final response = await innerHandler(request);
+      print(request.headers);
+
+      // Set CORS when responding to OPTIONS request
+      if (request.method == 'OPTIONS') {
+        return Response.ok('', headers: corsHeaders);
+      }
+
+      // Move onto handler
+      return response;
+    };
+  }).addHandler(app);
+
+  await io.serve(app, '51.79.251.248', port);
+  // await io.serve(app, 'localhost', port);
 }
